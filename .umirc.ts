ba9059cf1d9b9fbb39e874ea37e8a81840dee6ad
asdrtLin <86903002+asdrtLin@users.noreply.github.com>
2023-05-10 12:56:06 +0800

    fix(core): handle events on blur (close #28)

---
+++ .umirc.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '@umijs/max',
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      name: '首页',
+      path: '/home',
+      component: './Home',
+    },
+    {
+      name: '权限演示',
+      path: '/access',
+      component: './Access',
+    },
+    {
+      name: ' CRUD 示例',
+      path: '/table',
+      component: './Table',
+    },
+    {
+      name: 'vue3',
+      path: '/vue3/*',
+      microApp: 'vue3',
+    },
+  ],
+  qiankun: {
+    master: {
+      apps: [
+        {
+          name: 'vue3',
+          entry: '//localhost:7105',
+        },
+      ],
+    },
+  },
+  npmClient: 'yarn',
+});
